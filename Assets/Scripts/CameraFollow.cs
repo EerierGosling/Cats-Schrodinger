4fe5dfa3228df802d11b59f4d0cb1fe46b59e572
TheLazySquid <76746384+TheLazySquid@users.noreply.github.com>
2023-07-08 11:59:49 -0400

    Clamp Camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,16 +7,20 @@ public class CameraFollow : MonoBehaviour
     public float FollowSpeed = 5f;
     public Transform target;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    public float maxLeft;
+    public float maxRight;
+    public float maxUp;
+    public float maxDown;
 
     // Update is called once per frame
     void Update()
     {
         Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
+
+        // clamp the camera to the bounds
+        newPos.x = Mathf.Clamp(newPos.x, maxLeft, maxRight);
+        newPos.y = Mathf.Clamp(newPos.y, maxDown, maxUp);
+
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
     }
 }
