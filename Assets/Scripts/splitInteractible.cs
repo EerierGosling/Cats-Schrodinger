a8330444223c8ed2376535c50000e7e0317da617
TheLazySquid <76746384+TheLazySquid@users.noreply.github.com>
2023-07-08 12:53:20 -0400

    Interaction Prompt

--- Assets/Scripts/splitInteractible.cs
+++ Assets/Scripts/splitInteractible.cs
@@ -8,7 +8,9 @@ public class splitInteractible : MonoBehaviour
     private TimelineManager timelineManager;
     private OptionShower optionShower;
     private GameObject cat;
+    private GameObject prompt;
 
+    public GameObject promptPrefab;
     public string text1;
     public string text2;
     public string load1;
@@ -20,18 +22,26 @@ public class splitInteractible : MonoBehaviour
         cat = GameObject.FindGameObjectWithTag("Player");
         timelineManager = GameObject.FindObjectOfType<TimelineManager>();
         optionShower = GameObject.FindObjectOfType<OptionShower>();
+
+        prompt = Instantiate(promptPrefab, transform.position, Quaternion.identity);
+        // place it above the object
+        prompt.transform.position += new Vector3(0, 1f, 0);
     }
 
     void Update()
     {
+        // get the distance to the player
+        float distance = Vector3.Distance(cat.transform.position, transform.position);
+
+        // if the player is close enough, show the prompt
+        prompt.SetActive(distance < 1.5f);
+
         if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
         {
-            // get the distance to the player
-            float distance = Vector3.Distance(cat.transform.position, transform.position);
 
             // if the player is close enough, interact
-            // if(distance < 1.5f) Interact();
-            Interact();
+            if(distance < 1.5f) Interact();
+            // Interact();
         }
     }
 
