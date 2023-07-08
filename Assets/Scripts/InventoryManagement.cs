bef41ffeded2566bc68163fce03cddcf382c97d2
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2023-07-08 13:39:41 -0400

    inventory functionality (1/2)

---
+++ Assets/Scripts/InventoryManagement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManagement : MonoBehaviour
+{
+    public GameObject inv1;
+    public GameObject inv2;
+    public GameObject inv3;
+
+    public List inventoryItems = new List[]; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
