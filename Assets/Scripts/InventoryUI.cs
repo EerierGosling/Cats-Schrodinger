5dc9b9894aeeb520eb105880125173ee9dd370b3
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2023-07-08 15:48:14 -0400

    inventory (2/godknowshowmany)

--- Assets/Scripts/InventoryUI.cs
+++ Assets/Scripts/InventoryUI.cs
@@ -2,14 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventoryManagement : MonoBehaviour
+public class InventoryUI : MonoBehaviour
 {
-    public GameObject inv1;
-    public GameObject inv2;
-    public GameObject inv3;
-
-    public List inventoryItems = new List[]; 
-
     // Start is called before the first frame update
     void Start()
     {
