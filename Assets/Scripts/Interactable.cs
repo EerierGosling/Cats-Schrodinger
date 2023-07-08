c3fd5fae042781c65a8dcab7776f57106ca359a7
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2023-07-08 17:07:52 -0400

    LET'S FREAKIN' GOOOO

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -3,6 +3,27 @@ using UnityEngine;
 public class Interactable : MonoBehaviour
 {
     public float radius = 3f;
+    bool isFocus = false;
+    Transform player;
+
+    void Update () {
+        if (isFocus) {
+            float distance = Vector3.Distance(player.position, transform.position);
+            if (distance <= radius) {
+                Debug.Log("Interact");
+            }
+        }
+    }
+
+    public void OnFocused (Transform playerTransform) {
+        isFocus = true;
+        player = playerTransform;
+    }
+
+    public void OnDefocused () {
+        isFocus = false;
+        player = null;
+    }
 
     void OnDrawGizmosSelected () {
         Gizmos.color = Color.yellow;
