ba5e7f9c9ac8e30524cc43a6af1bba0d32f665f1
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2023-07-11 13:01:34 -0400

    more stuff

---
+++ Assets/Scripts/TiltChairInteractable.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TiltChairInteractable : MonoBehaviour
+{
+    public GameObject promptPrefab;
+    public string pickupText;
+    public string grantItem;
+    public Sprite grantItemSprite;
+    public GameObject safeCode;
+
+    private InventoryManager invManager;
+    private GameObject prompt;
+    private GameObject cat;
+
+    void Awake()
+    {
+        cat = GameObject.FindGameObjectWithTag("Player");
+        invManager = FindObjectOfType<InventoryManager>();
+
+        prompt = Instantiate(promptPrefab, transform.position, Quaternion.identity);
+
+        prompt.transform.position += new Vector3(0, 1f, 0);
+        prompt.transform.parent = transform;
+    }
+
+    void Update()
+    {
+        if(!prompt) return;
+
+        // get the distance to the player
+        float distance = Vector3.Distance(cat.transform.position, transform.position);
+
+        // if the player is close enough, show the prompt
+        prompt.SetActive(distance < 1.5f);
+
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
+        {
+            // if the player is close enough, interact
+            if(distance < 1.5f) Interact();
+        }
+    }
+
+    void Interact()
+    {
+        invManager.AddItem(grantItem, grantItemSprite);
+        Destroy(safeCode);
+
+        invManager.ShowPopup(pickupText, 5f);
+    }
+}
